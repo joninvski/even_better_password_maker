fd3fda1d57d05d9a487ad908e4309972a8fd779e
Joao Trindade <trindade.joao@gmail.com>
2012-11-26 22:43:38 +0000

    Worked a little bit on profile detail

--- src/com/passwordmaker/ListProfileFrag.java
+++ src/com/passwordmaker/ListProfileFrag.java
@@ -23,54 +23,12 @@ public class ListProfileFrag extends ListFragment {
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
-
-        super.onActivityCreated(savedInstanceState);
-        String[] values = new String[] { "Enterprise", "Star Trek", "Next Generation", "Deep Space 9", "Voyager"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, values);
-        setListAdapter(adapter);
-
-        // Check to see if we have a frame in which to embed the details
-        //         // fragment directly in the containing UI.
-        View detailsFrame = getActivity().findViewById(R.id.frag_update_detail);
-        mDualPane = detailsFrame != null && detailsFrame.getVisibility() == View.VISIBLE;
-
         Log.v(TAG, "mDualPane= " + mDualPane);
     }
 
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
         Log.v(TAG, "Set Text = " + position);
-        String item = (String) getListAdapter().getItem(position);
-        DetailProfileFrag frag = (DetailProfileFrag) getFragmentManager().findFragmentById(R.id.frag_update_detail);
-        if (frag != null && frag.isInLayout()) {
-            frag.setText(getCapt(item));
-
-        }
-
-        if (mDualPane) {
-            // We can display everything in-place with fragments.
-            // Have the list highlight this item and show the data.
-            getListView().setItemChecked(position, true);
-
-            // Check what fragment is shown, replace if needed.
-            DetailProfileFrag details = (DetailProfileFrag)
-                getFragmentManager().findFragmentById(R.id.frag_update_detail);
-            if (details == null) {
-                Log.v(TAG, "Details is null");
-                // Make new fragment to show this selection.
-                details = DetailProfileFrag.newInstance(position);
-
-                // Execute a transaction, replacing any existing
-                // fragment with this one inside the frame.
-                FragmentTransaction ft
-                    = getFragmentManager().beginTransaction();
-                ft.replace(R.id.frag_update_detail, details);
-                ft.setTransition(
-                        FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-                ft.commit();
-            }
-
-        }
     }
 
     @Override
@@ -79,22 +37,4 @@ public class ListProfileFrag extends ListFragment {
         return view;
     }
 
-    private String getCapt(String ship) {
-        if (ship.toLowerCase().contains("enterprise")) {
-            return "Johnathan Archer";
-        }
-        if (ship.toLowerCase().contains("star trek")) {
-            return "James T. Kirk";
-        }
-        if (ship.toLowerCase().contains("next generation")) {
-            return "Jean-Luc Picard";
-        }
-        if (ship.toLowerCase().contains("deep space 9")) {
-            return "Benjamin Sisko";
-        }
-        if (ship.toLowerCase().contains("voyager")) {
-            return "Kathryn Janeway";
-        }
-        return "???";
-    }
 }
