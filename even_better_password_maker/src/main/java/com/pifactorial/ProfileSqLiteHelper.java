c86ce1698d1e92ef95e63baf559fb5d00eb07427
Joao Trindade <trindade.joao@gmail.com>
2013-04-01 20:28:15 +0100

    More DB parameters

--- even_better_password_maker/src/main/java/com/pifactorial/ProfileSqLiteHelper.java
+++ even_better_password_maker/src/main/java/com/pifactorial/ProfileSqLiteHelper.java
@@ -9,36 +9,56 @@ public class ProfileSqLiteHelper extends SQLiteOpenHelper {
 
 	private static final String TAG = "ProfileSqlLiteHelper";
 
-	public static final String TABLE_PROFILES    = "profiles";
-	public static final String COLUMN_ID         = "_id";
-	public static final String COLUMN_NAME       = "name";
-	public static final String COLUMN_USERNAME   = "username";
-	public static final String COLUMN_ALGORITHM  = "algorithm";
-	public static final String COLUMN_LENGTH     = "length";
-	public static final String COLUMN_LEET_TYPE  = "leet_type";
-	public static final String COLUMN_LEET_LEVEL = "leet_level";
-	public static final String COLUMN_MODIFIER   = "modifiers";
-	public static final String COLUMN_PREFIX     = "prefix";
-	public static final String COLUMN_SUFFIX     = "suffix";
-    // TODO CHARACTER SET
-    // TODO URL COMPONENTS
+	public static final String TABLE_PROFILES                       = "profiles";
+	public static final String COLUMN_ID                            = "_id";
+	public static final String COLUMN_NAME                          = "name";
+	public static final String COLUMN_USERNAME                      = "username";
+	public static final String COLUMN_ALGORITHM                     = "algorithm";
+	public static final String COLUMN_LENGTH                        = "length";
+	public static final String COLUMN_LEET_TYPE                     = "leet_type";
+	public static final String COLUMN_LEET_LEVEL                    = "leet_level";
+	public static final String COLUMN_MODIFIER                      = "modifiers";
+	public static final String COLUMN_PREFIX                        = "prefix";
+	public static final String COLUMN_SUFFIX                        = "suffix";
+
+    // URL COMPONENTS
+    public static final String COLUMN_URL_COMPONENT_PROTOCOL        = "url_comp_protocol"
+    public static final String COLUMN_URL_COMPONENT_SUBDOMAIN       = "url_comp_subdomain"
+    public static final String COLUMN_URL_COMPONENT_DOMAIN          = "url_comp_domain"
+    public static final String COLUMN_URL_COMPONENT_PORT_PARAMETERS = "url_comp_port"
+
+    // CHARACTER SET
+    public static final String COLUMN_CHAR_SET_UPPERCASE            = "char_set_upper"
+    public static final String COLUMN_CHAR_SET_LOWERCASE            = "char_set_lower"
+    public static final String COLUMN_CHAR_SET_NUMBERS              = "char_set_numbers"
+    public static final String COLUMN_CHAR_SET_SYMBOLS              = "char_set_symbols"
+    public static final String COLUMN_CHAR_SET_COSTUM               = "char_set_costum"
 
 	private static final String DATABASE_NAME = "profiles.db";
 	private static final int DATABASE_VERSION = 1;
 
 	// Database creation sql statement
 	private static final String DATABASE_CREATE = "create table "
-			+ TABLE_PROFILES    + "("
-			+ COLUMN_ID         + " integer primary key autoincrement, "
-			+ COLUMN_NAME       + " text not null, "
-			+ COLUMN_USERNAME   + " text not null, "
-			+ COLUMN_ALGORITHM  + " text not null, "
-			+ COLUMN_LENGTH     + " integer not null, "
-			+ COLUMN_LEET_TYPE  + " text not null, "
-			+ COLUMN_LEET_LEVEL + " integer not null, "
-			+ COLUMN_MODIFIER   + " text not null, "
-			+ COLUMN_PREFIX     + " text not null, "
-			+ COLUMN_SUFFIX     + " text not null "
+			+ TABLE_PROFILES                       + "("
+			+ COLUMN_ID                            + " integer primary key autoincrement, "
+			+ COLUMN_NAME                          + " text not null, "
+			+ COLUMN_USERNAME                      + " text not null, "
+			+ COLUMN_ALGORITHM                     + " text not null, "
+			+ COLUMN_LENGTH                        + " integer not null, "
+			+ COLUMN_LEET_TYPE                     + " text not null, "
+			+ COLUMN_LEET_LEVEL                    + " integer not null, "
+			+ COLUMN_MODIFIER                      + " text not null, "
+			+ COLUMN_PREFIX                        + " text not null, "
+			+ COLUMN_SUFFIX                        + " text not null, "
+			+ COLUMN_URL_COMPONENT_PROTOCOL        + " boolean not null, "
+			+ COLUMN_URL_COMPONENT_SUBDOMAIN       + " boolean not null, "
+			+ COLUMN_URL_COMPONENT_DOMAIN          + " boolean not null, "
+			+ COLUMN_URL_COMPONENT_PORT_PARAMETERS + " boolean not null "
+			+ COLUMN_CHAR_SET_UPPERCASE            + " boolean not null "
+			+ COLUMN_CHAR_SET_LOWERCASE            + " boolean not null "
+			+ COLUMN_CHAR_SET_NUMBERS              + " boolean not null "
+			+ COLUMN_CHAR_SET_SYMBOLS              + " boolean not null "
+			+ COLUMN_CHAR_SET_COSTUM               + " text not null "
 			+ ");";
 
 	public ProfileSqLiteHelper(Context context) {
