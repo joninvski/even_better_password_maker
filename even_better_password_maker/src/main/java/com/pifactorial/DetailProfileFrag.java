9fea7470085ec85e09d19d92f619c78aea55a76d
Joao Trindade <trindade.joao@gmail.com>
2013-04-02 01:15:52 +0100

    Added save and delete to profile detail

--- even_better_password_maker/src/main/java/com/pifactorial/DetailProfileFrag.java
+++ even_better_password_maker/src/main/java/com/pifactorial/DetailProfileFrag.java
@@ -3,65 +3,71 @@ package com.pifactorial;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.util.Log;
 
-
 public class DetailProfileFrag extends Fragment {
 
-    private static final String TAG = "DetailProfileFrag";
+	private static final String TAG = "DetailProfileFrag";
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-    	Log.v(TAG, "Detail Profile created");
-    	super.onCreate(savedInstanceState);
-        
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		Log.v(TAG, "Detail Profile created");
+		super.onCreate(savedInstanceState);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        Log.v(TAG, "ViewCreated");
-    	View view = inflater.inflate(R.layout.detail_profile, container, false);
-        return view;
-    }
+	}
 
-    
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        Log.v(TAG, "Activity");
-    }
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		Log.v(TAG, "ViewCreated");
+		View view = inflater.inflate(R.layout.detail_profile, container, false);
+		return view;
+	}
 
-  
-    public void setText(String item) {
-        Log.v(TAG, "Set Text = " + item);
-        TextView view = (TextView) getView().findViewById(R.id.profile_name);
-        view.setText(item);
-    }
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		Log.v(TAG, "Activity");
+	}
 
-    @Override
-    public void onStop() {
-    	super.onStop();
-    };
-    
-    public int getShownIndex() {
-        Log.v(TAG, "Get shown index = ");
+	public void setText(String item) {
+		Log.v(TAG, "Set Text = " + item);
+		TextView view = (TextView) getView().findViewById(R.id.profile_name);
+		view.setText(item);
+	}
 
-    	return getArguments().getInt("index", 0);
-    }
+	@Override
+	public void onStop() {
+		super.onStop();
+	};
 
-    public static DetailProfileFrag newInstance(int index) {
-        Log.v(TAG, "New instance = ");
+	public int getShownIndex() {
+		Log.v(TAG, "Get shown index = ");
 
-    	DetailProfileFrag f = new DetailProfileFrag();
+		return getArguments().getInt("index", 0);
+	}
 
-        // Supply index input as an argument.
-        Bundle args = new Bundle();
-        args.putInt("index", index);
-        f.setArguments(args);
+	public static DetailProfileFrag newInstance(int index) {
+		Log.v(TAG, "New instance = ");
 
-        return f;
-    }    
+		DetailProfileFrag f = new DetailProfileFrag();
+
+		// Supply index input as an argument.
+		Bundle args = new Bundle();
+		args.putInt("index", index);
+		f.setArguments(args);
+
+		return f;
+	}
+
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		inflater.inflate(R.menu.main_menu, menu);
+		super.onCreateOptionsMenu(menu, inflater);
+	};
 }
--- even_better_password_maker/src/main/java/com/pifactorial/UpdateActivity.java
+++ even_better_password_maker/src/main/java/com/pifactorial/UpdateActivity.java
@@ -2,13 +2,21 @@ package com.pifactorial;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class UpdateActivity extends Activity {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.profile);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.profile_menu, menu);
+		return super.onCreateOptionsMenu(menu);
 	}
 }
