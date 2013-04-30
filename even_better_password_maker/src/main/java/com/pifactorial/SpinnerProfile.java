999d98984960771fafb4e357dca6284e5ded823d
Joao Trindade <trindade.joao@gmail.com>
2013-04-30 19:49:22 +0100

    Added the missing SpinnerProfile class

---
+++ even_better_password_maker/src/main/java/com/pifactorial/SpinnerProfile.java
@@ -0,0 +1,17 @@
+package com.pifactorial;
+
+public class SpinnerProfile {
+    private final String a;
+    private final String b;
+
+    public SpinnerProfile(String a, String b)
+    {
+        this.a = a;
+        this.b = b;
+    }
+
+    @Override
+    public String toString(){
+        return this.a + " - " + this.b;
+    }
+}
