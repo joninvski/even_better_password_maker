a279eda270e486ddf668b776c3cf90e3d26e3453
Joao Trindade <trindade.joao@gmail.com>
2013-10-20 21:31:06 +0100

    Remove accept license script. Doing in travis.yml

--- accept_license.sh
+++
@@ -1,3 +0,0 @@
-expect -c 'set timeout -1; spawn android update sdk --filter 1,2,3,8,37,extra-android-support,extra-android-m2repository --no-ui --force ; expect {
-  "Do you accept the license" { exp_send "y\r" ; exp_continue }
-  eof }'
