9cbaa7586a912abd7119979ed984c7817c8eb1c0
Joao Trindade <trindade.joao@gmail.com>
2014-03-15 20:49:53 +0000

    onResume now stores url

--- src/main/java/com/pifactorial/EntryActivity.java
+++ src/main/java/com/pifactorial/EntryActivity.java
@@ -1,98 +1,105 @@
 package com.pifactorial;
 
+import android.app.Activity;
+
+import android.content.ClipboardManager;
+import android.content.ClipData;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteCursor;
+
+import android.net.Uri;
+
+import android.os.Bundle;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+
+import android.util.Log;
+
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.EditText;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import org.daveware.passwordmaker.PasswordGenerationException;
 import org.daveware.passwordmaker.PasswordMaker;
 import org.daveware.passwordmaker.Profile;
 import org.daveware.passwordmaker.SecureCharArray;
 
-import android.app.Activity;
-import android.content.ClipData;
-import android.content.ClipboardManager;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteCursor;
-import android.net.Uri;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.EditText;
-import android.widget.SimpleCursorAdapter;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.Toast;
-
 public class EntryActivity extends Activity implements View.OnClickListener {
 
-	private ProfileDataSource datasource;
-	private Boolean visible;
-	protected EditText etURL;
-	protected EditText etMasterPass;
-	protected TextView textOutputPass;
-	protected Spinner spProfiles;
+    private ProfileDataSource datasource;
+    private Boolean visible;
+    protected EditText etURL;
+    protected EditText etMasterPass;
+    protected TextView textOutputPass;
+    protected Spinner spProfiles;
 
-	private SharedPreferences mPrefs;
+    private SharedPreferences mPrefs;
 
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		Log.d(Constants.LOG, "Creating Entry Activity");
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Log.d(Constants.LOG, "Creating Entry Activity");
 
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
 
-		// Let's get the window controls
-		textOutputPass = (TextView) findViewById(R.id.tvResultPass);
-		etURL = (EditText) findViewById(R.id.etURL);
-		etURL.setSelection(etURL.getText().length()); // Puts the cursor at the
-														// end of the string
-		etMasterPass = (EditText) findViewById(R.id.etMasterPass);
-		spProfiles = (Spinner) findViewById(R.id.spProfiles);
-		Log.d(Constants.LOG, "Fetched all views");
+        // Let's get the window controls
+        textOutputPass = (TextView) findViewById(R.id.tvResultPass);
+        etURL = (EditText) findViewById(R.id.etURL);
 
-		// Set the action bar to show the app title
-		this.getActionBar().setDisplayShowTitleEnabled(true);
+        etMasterPass = (EditText) findViewById(R.id.etMasterPass);
+        spProfiles = (Spinner) findViewById(R.id.spProfiles);
+        Log.d(Constants.LOG, "Fetched all views");
 
-		// Master password should always start as not visible
-		visible = false;
+        // Set the action bar to show the app title
+        this.getActionBar().setDisplayShowTitleEnabled(true);
 
-		// Create a data source to get profiles
-		Log.d(Constants.LOG, "Creating data source");
-		datasource = new ProfileDataSource(this);
-		datasource.open();
-		Log.d(Constants.LOG, "Created data source");
+        // Master password should always start as not visible
+        visible = false;
 
-		mPrefs = getSharedPreferences(
-				getString(R.string.SharedPreferencesName), Context.MODE_PRIVATE);
+        // Create a data source to get profiles
+        Log.d(Constants.LOG, "Creating data source");
+        datasource = new ProfileDataSource(this);
+        datasource.open();
+        Log.d(Constants.LOG, "Created data source");
 
-		// Let's create the callback when the spinner changes.
-		// We just want to store in the last selected preference the selected
-		// value
-		spProfiles.setOnItemSelectedListener(new OnItemSelectedListener() {
-			public void onItemSelected(AdapterView<?> parentView,
-					View selectedItemView, int position, long id) {
-				Log.d(Constants.LOG, "Saving shared preferences");
+        mPrefs = getSharedPreferences(getString(R.string.SharedPreferencesName), Context.MODE_PRIVATE);
 
-				int last_selected = spProfiles.getSelectedItemPosition();
-				Editor editor = mPrefs.edit();
-				editor.putInt(getString(R.string.LastSelectedProfile),
-						last_selected);
-				editor.apply(); // TODO - Check the return value
-			}
+        // Let's create the callback when the spinner changes.
+        // We just want to store in the last selected preference the selected
+        // value
+        spProfiles.setOnItemSelectedListener(new OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parentView,
+                View selectedItemView, int position, long id) {
+                Log.d(Constants.LOG, "Saving shared preferences");
 
-			public void onNothingSelected(AdapterView<?> parentView) {
-				Log.i(Constants.LOG,
-						"Strange, nothing was selected on the profile spinner");
-			}
+                int last_selected = spProfiles.getSelectedItemPosition();
+                Editor editor = mPrefs.edit();
+                editor.putInt(getString(R.string.LastSelectedProfile), last_selected);
+                editor.apply(); // TODO - Check the return value
+            }
 
-		});
+            public void onNothingSelected(AdapterView<?> parentView) {
+                Log.i(Constants.LOG,
+                    "Strange, nothing was selected on the profile spinner");
+            }
+
+        });
 
         // Get intent, action and MIME type
         Intent intent = getIntent();
@@ -103,164 +110,186 @@ public class EntryActivity extends Activity implements View.OnClickListener {
             if ("text/plain".equals(type)) {
                 String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
                 if (sharedText != null) {
-                etURL.setText(sharedText);
+                    etURL.setText(sharedText);
                 }
             }
         }
 
-		// Check if there is no profile
-		if (datasource.getAllProfiles().size() < 1) {
-			Log.d(Constants.LOG, "No profile in DB was found");
-			Profile defaultProfile = Profile.getDefaultProfile();
-			Log.d(Constants.LOG, "Inserting default profile: " + defaultProfile);
-			datasource.insertProfile(defaultProfile);
-		}
+        // Check if there is no profile
+        if (datasource.getAllProfiles().size() < 1) {
+            Log.d(Constants.LOG, "No profile in DB was found");
+            Profile defaultProfile = Profile.getDefaultProfile();
+            Log.d(Constants.LOG, "Inserting default profile: " + defaultProfile);
+            datasource.insertProfile(defaultProfile);
+        }
 
-		updateProfileSpinner();
-	}
+        updateProfileSpinner();
 
-	public void updateProfileSpinner() {
-		// Populate a spinner with the profiles
-		Cursor cursor = datasource.getAllProfilesCursor();
-		SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
-				android.R.layout.simple_spinner_item, cursor,
-				new String[] { ProfileSqLiteHelper.COLUMN_NAME },
-				new int[] { android.R.id.text1 }, 0);
+        TextWatcher watcher = new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                updatePassword();
+            }
 
-		// Specify the layout to use when the list of choices appears
-		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		// Apply the adapter to the spinner
-		spProfiles.setAdapter(adapter);
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                return; // Do nothing
 
-		// Now let's get the default profile
-		int last_selected = mPrefs.getInt(
-				getString(R.string.LastSelectedProfile), 0);
-		spProfiles.setSelection(last_selected);
-	}
+            }
 
-	public void onClick(View v) {
-		switch (v.getId()) {
+            @Override
+            public void afterTextChanged(Editable s) {
+                return; // Do nothing
+            }
+        };
 
-		case R.id.tvResultPass:
+        // Change the passwords as the text views are changed
+        etURL.addTextChangedListener(watcher);
+        etMasterPass.addTextChangedListener(watcher);
+    }
 
-			Log.d(Constants.LOG, "Clicked on text output password");
+    public void updateProfileSpinner() {
+        // Populate a spinner with the profiles
+        Cursor cursor = datasource.getAllProfilesCursor();
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+                android.R.layout.simple_spinner_item, cursor,
+                new String[] { ProfileSqLiteHelper.COLUMN_NAME },
+                new int[] { android.R.id.text1 }, 0);
 
-			// Toggle the visible variable
-			visible = !visible;
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spProfiles.setAdapter(adapter);
 
-			if (visible) {
-				try {
-					SecureCharArray result = getPassword();
+        // Now let's get the default profile
+        int last_selected = mPrefs.getInt(getString(R.string.LastSelectedProfile), 0);
+        spProfiles.setSelection(last_selected);
+    }
 
-					// Show the generated password
-					textOutputPass.setText(new String(result.getData()));
+    public void onClick(View v) {
+        switch (v.getId()) {
 
-				} catch (PasswordGenerationException e) {
-					Log.e(Constants.LOG, "Error in generating the new password"
-							+ e.getMessage());
-				}
+            case R.id.tvResultPass:
 
-			} else {
-				textOutputPass.setText("");
-			}
-			break;
+                Log.d(Constants.LOG, "Clicked on text output password");
+                // Toggle the visible variable
+                visible = !visible;
+                updatePassword();
 
-		default:
-			Log.e(Constants.LOG, "Some unexpected view was pressed");
-			break;
-		}
-	}
+                break;
 
-	private SecureCharArray getPassword() throws PasswordGenerationException {
-		// Get spinner profile
-		SQLiteCursor profileName = (SQLiteCursor) spProfiles.getSelectedItem();
-		Profile profile = datasource.cursorToAccount(profileName);
-		Log.d(Constants.LOG, "Profile fetched \n" + profile.toString());
+            default:
+                Log.e(Constants.LOG, "Some unexpected view was pressed");
+                break;
+        }
+    }
 
-		// Use the profile and master password to get the generated password
-		SecureCharArray master = new SecureCharArray(etMasterPass.getText()
-				.toString());
-		SecureCharArray result = PasswordMaker.makePassword(master, profile,
-				etURL.getText().toString());
-		return result;
-	}
+    private void updatePassword() {
+        if (visible) {
+            try {
+                SecureCharArray result = getPassword();
+                textOutputPass.setText(new String(result.getData()));     // Show the generated password
+            } catch (PasswordGenerationException e) {
+                Log.e(Constants.LOG, "Error in generating the new password"
+                        + e.getMessage());
+            }
+        } else {
+            textOutputPass.setText("");
+        }
+    }
 
-	@Override
-	protected void onResume() {
-		super.onResume();
-		updateProfileSpinner();
-	}
+    private SecureCharArray getPassword() throws PasswordGenerationException {
+        // Get spinner profile
+        SQLiteCursor profileName = (SQLiteCursor) spProfiles.getSelectedItem();
+        Profile profile = datasource.cursorToAccount(profileName);
+        Log.d(Constants.LOG, "Profile fetched \n" + profile.toString());
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle item selection
-		switch (item.getItemId()) {
+        // Use the profile and master password to get the generated password
+        SecureCharArray master = new SecureCharArray(etMasterPass.getText()
+                .toString());
+        SecureCharArray result = PasswordMaker.makePassword(master, profile,
+                etURL.getText().toString());
+        return result;
+    }
 
-		case R.id.actionBtnGo:
-			Log.d(Constants.LOG, "Pressed the go button");
-			Intent myWebLink = new Intent(android.content.Intent.ACTION_VIEW);
-			myWebLink.setData(Uri.parse(etURL.getText().toString()));
-			startActivity(myWebLink);
-			break;
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateProfileSpinner();
 
-		case R.id.actionBtnCopy:
-			Log.d(Constants.LOG, "Clicked item Copy");
-			try {
-				SecureCharArray generatedPassword = getPassword();
-			
+        String lastURL = mPrefs.getString("LastURL", getString(R.string.http));
+        etURL.setText(lastURL);
+        etURL.setSelection(etURL.getText().length()); // Puts the cursor at the end of the string
+    }
 
-			ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
-			ClipData clip = ClipData.newPlainText("Copied Text",
-					new String(generatedPassword.getData()));
-			clipboard.setPrimaryClip(clip);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
 
-			Toast.makeText(getApplicationContext(),
-					R.string.password_copy_to_clipboard, Toast.LENGTH_SHORT)
-					.show();
-			} catch (PasswordGenerationException e) {
-				Log.e(Constants.LOG,
-						"Error in generating the new password" + e.getMessage());
-				Toast.makeText(getApplicationContext(),
-						"Error generating password", Toast.LENGTH_SHORT)
-						.show();
-				
-			}
+            case R.id.actionBtnGo:
+                Log.d(Constants.LOG, "Pressed the go button");
+                Intent myWebLink = new Intent(android.content.Intent.ACTION_VIEW);
+                myWebLink.setData(Uri.parse(etURL.getText().toString()));
+                startActivity(myWebLink);
+                break;
 
-			break;
+            case R.id.actionBtnCopy:
+                Log.d(Constants.LOG, "Clicked item Copy");
+                try {
+                    SecureCharArray generatedPassword = getPassword();
 
-		// case R.id.actionBtnAbout:
-		// Log.i(Constants.LOG, "Clicked About");
-		// break;
+                    ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+                    ClipData clip = ClipData.newPlainText("Copied Text", new String(generatedPassword.getData()));
+                    clipboard.setPrimaryClip(clip);
 
-		case R.id.actionBtnProfiles:
-			Log.d(Constants.LOG, "Clicked Profiles");
-			Intent myIntent = new Intent(EntryActivity.this,
-					UpdateActivity.class);
-			EntryActivity.this.startActivity(myIntent);
-			break;
+                    Toast.makeText(getApplicationContext(), R.string.password_copy_to_clipboard, Toast.LENGTH_SHORT).show();
+                } catch (PasswordGenerationException e) {
+                    Log.e(Constants.LOG, "Error in generating the new password" + e.getMessage());
+                    Toast.makeText(getApplicationContext(), "Error generating password", Toast.LENGTH_SHORT).show();
+                }
 
-		default:
-			return super.onOptionsItemSelected(item);
-		}
-		return true;
-	}
+                break;
 
-	@Override
-	protected void onPause() {
-		Log.d(Constants.LOG, "on Pausing");
-		super.onPause();
-	}
+                // case R.id.actionBtnAbout:
+                // Log.i(Constants.LOG, "Clicked About");
+                // break;
 
-	@Override
-	protected void onStop() {
-		Log.d(Constants.LOG, "on Stopping");
-		super.onStop();
-	}
+            case R.id.actionBtnProfiles:
+                Log.d(Constants.LOG, "Clicked Profiles");
+                Intent myIntent = new Intent(EntryActivity.this,
+                        UpdateActivity.class);
+                EntryActivity.this.startActivity(myIntent);
+                break;
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.main_menu, menu);
-		return super.onCreateOptionsMenu(menu);
-	}
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(Constants.LOG, "on Pausing");
+
+        // Save the last URL
+        Editor editor = mPrefs.edit();
+        editor.putString("LastURL", etURL.getText().toString());
+        editor.apply(); // TODO - Check the return value
+
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(Constants.LOG, "on Stopping");
+        super.onStop();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 }
