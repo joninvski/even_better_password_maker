3c902e8da9502004b3f6ccdb8c5ad95e77e9415d
Joao Trindade <trindade.joao@gmail.com>
2014-04-26 17:10:22 +0100

    Add spoon to the test

--- app/src/androidTest/java/com/pifactorial/test/EntryActivityTest.java
+++ app/src/androidTest/java/com/pifactorial/test/EntryActivityTest.java
@@ -2,16 +2,30 @@ package com.pifactorial.test;
 
 import android.test.ActivityInstrumentationTestCase2;
 import com.pifactorial.*;
+import com.squareup.spoon.Spoon;
+import com.robotium.solo.*;
 
 public class EntryActivityTest extends ActivityInstrumentationTestCase2<EntryActivity> {
+    private Solo solo;
 
     public EntryActivityTest() {
         super(EntryActivity.class);
     }
 
-    public void testActivity() {
+    public void setUp() throws Exception {
+        solo = new Solo(getInstrumentation());
+        getActivity();
+    }
+
+    public void testEntryActivity() {
+
+        int timeout = 5000;
+
+        // Wait for activity: 'course.labs.fragmentslab.MainActivity'
+        assertTrue("MainActivity not found", solo.waitForActivity(
+                       com.pifactorial.EntryActivity.class, timeout));
+
         EntryActivity activity = getActivity();
-        assertNotNull(activity);
+        Spoon.screenshot(activity, "initial_state");
     }
 }
-
--- app/src/androidTest/java/com/pifactorial/test/UpdateActivityTest.java
+++ app/src/androidTest/java/com/pifactorial/test/UpdateActivityTest.java
@@ -2,16 +2,31 @@ package com.pifactorial.test;
 
 import android.test.ActivityInstrumentationTestCase2;
 import com.pifactorial.*;
+import com.squareup.spoon.Spoon;
+import com.robotium.solo.*;
 
 public class UpdateActivityTest extends ActivityInstrumentationTestCase2<UpdateActivity> {
+    private Solo solo;
 
     public UpdateActivityTest() {
         super(UpdateActivity.class);
     }
 
-    public void testActivity() {
+    public void setUp() throws Exception {
+        solo = new Solo(getInstrumentation());
+        getActivity();
+    }
+
+    public void testUpdateActivity() {
+
+        int timeout = 5000;
+
+        // Wait for activity: 'course.labs.fragmentslab.MainActivity'
+        assertTrue("Update activity not found", solo.waitForActivity(
+                       com.pifactorial.UpdateActivity.class, timeout));
+
         UpdateActivity activity = getActivity();
-        assertNotNull(activity);
+        Spoon.screenshot(activity, "update_activity_start");
     }
 }
 
