93cbd003ee3f80b189df4ba796faeebedf239f86
Joao Trindade <trindade.joao@gmail.com>
2014-06-02 00:44:51 +0100

    Add toplevel domains special case (no-ui yet)

--- domain/src/main/java/org/daveware/passwordmaker/PasswordMaker.java
+++ domain/src/main/java/org/daveware/passwordmaker/PasswordMaker.java
@@ -56,7 +56,7 @@ public class PasswordMaker {
      *             On odd length!?
      */
     public static SecureCharArray rstr2any(char[] input, String encoding)
-    throws Exception {
+        throws Exception {
         int length = input.length;
         int divisor;
         int full_length;
@@ -77,13 +77,12 @@ public class PasswordMaker {
         dividend_length = (int) Math.ceil((double) length / 2.0);
         dividend = new int[dividend_length];
         for (i = 0; i < dividend_length; i++) {
-            dividend[i] = (((int) input[i * 2]) << 8)
-                          | ((int) input[i * 2 + 1]);
+            dividend[i] = (((int) input[i * 2]) << 8) | ((int) input[i * 2 + 1]);
         }
 
         full_length = (int) Math.ceil(
-                          (double) length * 8
-                          / (Math.log((double) encoding.length()) / Math.log((double) 2)));
+                (double) length * 8 /
+                (Math.log((double) encoding.length()) / Math.log((double) 2)));
         remainders = new int[full_length];
 
         for (j = 0; j < full_length; j++) {
@@ -138,35 +137,72 @@ public class PasswordMaker {
             portPath = "";
 
         StringBuilder retVal = new StringBuilder(inputText.length());
-        if (uriComponents.contains(UrlComponents.Protocol)
-                && protocol.length() > 0) {
+        if (uriComponents.contains(UrlComponents.Protocol) && protocol.length() > 0) {
             retVal.append(protocol);
         }
+
         if (domainText != null) {
-            final String subDomain;
-            int dnDot = domainText.lastIndexOf('.');
-            dnDot = domainText.lastIndexOf('.', dnDot - 1);
-            if (dnDot != -1) {
-                subDomain = domainText.substring(0, dnDot);
-                domainText = domainText.substring(dnDot + 1);
-            } else {
-                subDomain = "";
-            }
-            final boolean hasSubDomain = uriComponents.contains(UrlComponents.Subdomain) && dnDot != -1;
+
+            final String topDomainText = calculateDomain(domainText);
+            final String subDomainText = calculateSubDomain(domainText, topDomainText);
+
+            final boolean hasSubDomain = uriComponents.contains(UrlComponents.Subdomain) && subDomainText != "";
             if (hasSubDomain) {
-                retVal.append(subDomain);
+                retVal.append(subDomainText);
             }
             if (uriComponents.contains(UrlComponents.Domain)) {
                 if (hasSubDomain)
                     retVal.append('.');
-                retVal.append(domainText);
+                retVal.append(topDomainText);
             }
         }
-        if (uriComponents.contains(UrlComponents.PortPathAnchorQuery)
-                && portPath.length() > 0) {
+
+        if (uriComponents.contains(UrlComponents.PortPathAnchorQuery) && portPath.length() > 0) {
             retVal.append(portPath);
         }
         return retVal.toString();
+    }
+
+    private static String calculateSubDomain(String domainText, String topDomainText) {
+        int lengthToRemove = topDomainText.length();
+        int rightmostSubdomainIndex = domainText.length() - lengthToRemove;
+        String subdomain = domainText.substring(0, rightmostSubdomainIndex);
+
+        return subdomain;
+    }
+
+    private static String calculateTopDomain(String domainText) {
+        for(String topDomain : TOPLEVELDOMAINS) {
+            if(domainText.endsWith(topDomain)) {
+                return topDomain;
+            }
+        }
+
+        // If it is not a special top domain then it is just the substring after the last dot
+        int lastDot = domainText.lastIndexOf('.');
+        String domain = domainText.substring(lastDot + 1);
+        return domain;
+    }
+
+    private static String calculateMiddleDomain(String domainText, String top) {
+        int lengthToRemove = top.length() + 1; // The +1 is for the dot
+        int rightmostSubdomainIndex = domainText.length() - lengthToRemove;
+        String domainAndSubDomain = domainText.substring(0, rightmostSubdomainIndex);
+
+        int lastDot = domainAndSubDomain.lastIndexOf('.');
+        String middleDomain = domainAndSubDomain.substring(lastDot + 1);
+
+        return middleDomain;
+    }
+
+    private static String calculateDomain(String domainText) {
+        String top = calculateTopDomain(domainText);
+        String middle = calculateMiddleDomain(domainText, top);
+
+        if (middle != "")
+            return middle + '.' + top;
+        else
+            return top;
     }
 
     /**
@@ -183,7 +219,7 @@ public class PasswordMaker {
      *             if something bad happened.
      */
     public static SecureCharArray makePassword(SecureCharArray masterPassword, Profile profile, final String inputText)
-    throws PasswordGenerationException {
+        throws PasswordGenerationException {
 
         LeetLevel leetLevel = profile.getLeetLevel();
         int length = profile.getLength();
@@ -193,18 +229,18 @@ public class PasswordMaker {
         try {
             if (profile.getCompleteCharacterSet().length() < 2)
                 throw new Exception(
-                    "profile contains a character set that is too short: " +
-                    profile.getCompleteCharacterSet());
+                        "profile contains a character set that is too short: " +
+                        profile.getCompleteCharacterSet());
 
             data = new SecureCharArray(getModifiedInputText(inputText, profile)
-                                       + profile.getUsername() + profile.getModifier());
+                    + profile.getUsername() + profile.getModifier());
 
             // Use leet before hashing
             if (profile.getLeetType() == LeetType.BEFORE
                     || profile.getLeetType() == LeetType.BOTH) {
                 LeetEncoder.leetConvert(leetLevel, masterPassword);
                 LeetEncoder.leetConvert(leetLevel, data);
-            }
+                    }
 
             // Perform the actual hashing
             output = hashTheData(masterPassword, data, profile);
@@ -213,12 +249,12 @@ public class PasswordMaker {
             if (profile.getLeetType() == LeetType.AFTER
                     || profile.getLeetType() == LeetType.BOTH) {
                 LeetEncoder.leetConvert(leetLevel, output);
-            }
+                    }
 
             // Apply the prefix
             if (profile.getPrefix().length() > 0) {
                 SecureCharArray prefix = new SecureCharArray(
-                    profile.getPrefix());
+                        profile.getPrefix());
                 output.prepend(prefix);
                 prefix.erase();
             }
@@ -227,7 +263,7 @@ public class PasswordMaker {
             output.resize(length, true);
             if (profile.getSuffix().length() > 0) {
                 SecureCharArray suffix = new SecureCharArray(
-                    profile.getSuffix());
+                        profile.getSuffix());
 
                 // If the suffix is larger than the entire password (not smart),
                 // then
@@ -284,16 +320,16 @@ public class PasswordMaker {
             while (output.size() < length) {
                 if (count == 0) {
                     intermediateOutput = runAlgorithm(masterPassword, data,
-                                                      profile);
+                            profile);
                 } else {
                     // add ye bit'o chaos
                     secureIteration.replace(masterPassword);
                     secureIteration.append(new SecureCharArray("\n"));
                     secureIteration.append(new SecureCharArray(Integer
-                                           .toString(count)));
+                                .toString(count)));
 
                     interIntermediateOutput = runAlgorithm(secureIteration,
-                                                           data, profile);
+                            data, profile);
                     intermediateOutput.append(interIntermediateOutput);
                     interIntermediateOutput.erase();
 
@@ -374,4 +410,80 @@ public class PasswordMaker {
 
         return output;
     }
+
+    static String[] TOPLEVELDOMAINS = {
+        "aland.fi", "wa.edu.au", "nsw.edu.au", "vic.edu.au", "csiro.au",
+        "conf.au", "info.au", "oz.au", "telememo.au", "sa.edu.au",
+        "nt.edu.au", "tas.edu.au", "act.edu.au", "wa.gov.au", "nsw.gov.au",
+        "vic.gov.au", "qld.gov.au", "sa.gov.au", "tas.gov.au", "nt.gov.au",
+        "act.gov.au", "archie.au", "edu.au", "gov.au", "id.au", "org.au",
+        "asn.au", "net.au", "com.au", "qld.edu.au", "com.bb", "net.bb",
+        "org.bb", "gov.bb", "agr.br", "am.br", "art.br", "edu.br",
+        "com.br", "coop.br", "esp.br", "far.br", "fm.br", "g12.br",
+        "gov.br", "imb.br", "ind.br", "inf.br", "mil.br", "net.br",
+        "org.br", "psi.br", "rec.br", "srv.br", "tmp.br", "tur.br",
+        "tv.br", "etc.br", "adm.br", "adv.br", "arq.br", "ato.br",
+        "bio.br", "bmd.br", "cim.br", "cng.br", "cnt.br", "ecn.br",
+        "eng.br", "eti.br", "fnd.br", "fot.br", "fst.br", "ggf.br",
+        "jor.br", "lel.br", "mat.br", "med.br", "mus.br", "not.br",
+        "ntr.br", "odo.br", "ppg.br", "pro.br", "psc.br", "qsl.br",
+        "slg.br", "trd.br", "vet.br", "zlg.br", "nom.br", "ab.ca",
+        "bc.ca", "mb.ca", "nb.ca", "nf.ca", "nl.ca", "ns.ca", "nt.ca",
+        "nu.ca", "on.ca", "pe.ca", "qc.ca", "sk.ca", "yk.ca", "com.cd",
+        "net.cd", "org.cd", "ac.cn", "com.cn", "edu.cn", "gov.cn",
+        "net.cn", "org.cn", "ah.cn", "bj.cn", "cq.cn", "fj.cn",
+        "gd.cn", "gs.cn", "gz.cn", "gx.cn", "ha.cn", "hb.cn", "he.cn",
+        "hi.cn", "hl.cn", "hn.cn", "jl.cn", "js.cn", "jx.cn", "ln.cn",
+        "nm.cn", "nx.cn", "qh.cn", "sc.cn", "sd.cn", "sh.cn", "sn.cn",
+        "sx.cn", "tj.cn", "xj.cn", "xz.cn", "yn.cn", "zj.cn", "co.ck",
+        "org.ck", "edu.ck", "gov.ck", "net.ck", "ac.cr", "co.cr",
+        "ed.cr", "fi.cr", "go.cr", "or.cr", "sa.cr", "eu.int", "ac.in",
+        "co.in", "edu.in", "firm.in", "gen.in", "gov.in", "ind.in",
+        "mil.in", "net.in", "org.in", "res.in", "ac.id", "co.id",
+        "or.id", "net.id", "web.id", "sch.id", "go.id", "mil.id",
+        "war.net.id", "ac.nz", "co.nz", "cri.nz", "gen.nz", "geek.nz",
+        "govt.nz", "iwi.nz", "maori.nz", "mil.nz", "net.nz", "org.nz",
+        "school.nz", "aid.pl", "agro.pl", "atm.pl", "auto.pl", "biz.pl",
+        "com.pl", "edu.pl", "gmina.pl", "gsm.pl", "info.pl", "mail.pl",
+        "miasta.pl", "media.pl", "nil.pl", "net.pl", "nieruchomosci.pl",
+        "nom.pl", "pc.pl", "powiat.pl", "priv.pl", "realestate.pl",
+        "rel.pl", "sex.pl", "shop.pl", "sklep.pl", "sos.pl", "szkola.pl",
+        "targi.pl", "tm.pl", "tourism.pl", "travel.pl", "turystyka.pl",
+        "com.pt", "edu.pt", "gov.pt", "int.pt", "net.pt", "nome.pt",
+        "org.pt", "publ.pt", "com.tw", "club.tw", "ebiz.tw", "game.tw",
+        "gov.tw", "idv.tw", "net.tw", "org.tw", "av.tr", "bbs.tr",
+        "bel.tr", "biz.tr", "com.tr", "dr.tr", "edu.tr", "gen.tr",
+        "gov.tr", "info.tr", "k12.tr", "mil.tr", "name.tr", "net.tr",
+        "org.tr", "pol.tr", "tel.tr", "web.tr", "ac.za", "city.za",
+        "co.za", "edu.za", "gov.za", "law.za", "mil.za", "nom.za",
+        "org.za", "school.za", "alt.za", "net.za", "ngo.za", "tm.za",
+        "web.za", "bourse.za", "agric.za", "cybernet.za", "grondar.za",
+        "iaccess.za", "inca.za", "nis.za", "olivetti.za", "pix.za",
+        "db.za", "imt.za", "landesign.za", "co.kr", "pe.kr", "or.kr",
+        "go.kr", "ac.kr", "mil.kr", "ne.kr", "chiyoda.tokyo.jp",
+        "tcvb.or.jp", "ac.jp", "ad.jp", "co.jp", "ed.jp", "go.jp",
+        "gr.jp", "lg.jp", "ne.jp", "or.jp", "com.mx", "net.mx",
+        "org.mx", "edu.mx", "gob.mx", "ac.uk", "co.uk", "gov.uk",
+        "ltd.uk", "me.uk", "mod.uk", "net.uk", "nic.uk", "nhs.uk",
+        "org.uk", "plc.uk", "police.uk", "sch.uk", "ak.us", "al.us",
+        "ar.us", "az.us", "ca.us", "co.us", "ct.us", "dc.us", "de.us",
+        "dni.us", "fed.us", "fl.us", "ga.us", "hi.us", "ia.us",
+        "id.us", "il.us", "in.us", "isa.us", "kids.us", "ks.us",
+        "ky.us", "la.us", "ma.us", "md.us", "me.us", "mi.us", "mn.us",
+        "mo.us", "ms.us", "mt.us", "nc.us", "nd.us", "ne.us", "nh.us",
+        "nj.us", "nm.us", "nsn.us", "nv.us", "ny.us", "oh.us", "ok.us",
+        "or.us", "pa.us", "ri.us", "sc.us", "sd.us", "tn.us", "tx.us",
+        "ut.us", "vt.us", "va.us", "wa.us", "wi.us", "wv.us", "wy.us",
+        "com.ua", "edu.ua", "gov.ua", "net.ua", "org.ua", "cherkassy.ua",
+        "chernigov.ua", "chernovtsy.ua", "ck.ua", "cn.ua", "crimea.ua",
+        "cv.ua", "dn.ua", "dnepropetrovsk.ua", "donetsk.ua", "dp.ua",
+        "if.ua", "ivano-frankivsk.ua", "kh.ua", "kharkov.ua", "kherson.ua",
+        "kiev.ua", "kirovograd.ua", "km.ua", "kr.ua", "ks.ua", "lg.ua",
+        "lugansk.ua", "lutsk.ua", "lviv.ua", "mk.ua", "nikolaev.ua",
+        "od.ua", "odessa.ua", "pl.ua", "poltava.ua", "rovno.ua", "rv.ua",
+        "sebastopol.ua", "sumy.ua", "te.ua", "ternopil.ua", "vinnica.ua",
+        "vn.ua", "zaporizhzhe.ua", "zp.ua", "uz.ua", "uzhgorod.ua",
+        "zhitomir.ua", "zt.ua", "ac.il", "co.il", "org.il", "net.il",
+        "k12.il", "gov.il", "muni.il", "idf.il", "co.im", "org.im", "com.sg"
+    };
 }
